4ae0ed2dee83c0a99da7b8fcbdfb6b0bd1e6b7a1
Ralf Schmitt <ralf@systemexit.de>
2019-04-05 10:34:37 +0200

    use secure HTTPS connection
    The server is now only reachable via HTTPS, so we need to change it in the
    client too.

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ set -e
 WS_USER=$(echo "$WS_USER" |sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
 WS_PASSWORD=$(echo "$WS_PASSWORD" |sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
 
-exec /usr/bin/env WS_SERVER="ws://$WS_USER:$WS_PASSWORD@142.93.106.131" node ./app.js
+exec /usr/bin/env WS_SERVER="wss://$WS_USER:$WS_PASSWORD@netstatstc1.trustlines.foundation" node ./app.js
