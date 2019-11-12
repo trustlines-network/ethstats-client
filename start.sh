19ee555b31b853694afbe86c39e0d79f35e46940
Ralf Schmitt <ralf@brainbot.com>
2019-11-12 15:00:34 +0100

    Use netstats.laika as default WS_HOST

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ set -e
 
 WS_USER=$(echo "$WS_USER" |sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
 WS_PASSWORD=$(echo "$WS_PASSWORD" |sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
-: "${WS_HOST:=netstatstc1.trustlines.foundation}"
+: "${WS_HOST:=netstats.laika.trustlines.foundation}"
 
 exec /usr/bin/env WS_SERVER="wss://$WS_USER:$WS_PASSWORD@$WS_HOST" node ./app.js
